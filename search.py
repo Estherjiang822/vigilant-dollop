0f2a58f1b0eaae9b6ecddeb152d60177a568d74e
Daynel-Kem <dani3lkimm2005@gmail.com>
2025-11-23 17:23:34 -0500

    changes to search

--- search.py
+++ search.py
@@ -1,14 +1,15 @@
 import chess
-import math
 from evaluate import evaluate
 
-def minimax(pos, depth, alpha, beta, max_player):
-    if depth == 0 or evaluate(pos) == False: # ig we make evaluate(checkmate position) = False? for now at least
-        return evaluate(pos) 
+# Max Player = True means the player is playing White pieces
+# Max Player = False means the player is playing Black pieces
+def minimax(board, depth, alpha, beta, max_player):
+    if depth == 0 or board.is_game_over():
+        return evaluate(board) 
     
     if max_player:
-        max_eval = -math.inf
-        for child in pos:
+        max_eval = -float("inf")
+        for child in board:
             evaluation = minimax(child, depth - 1, alpha, beta, False)
             max_eval = max(max_eval, evaluation)
             alpha = max(alpha, evaluation)
@@ -17,8 +18,8 @@ def minimax(pos, depth, alpha, beta, max_player):
         return max_eval
     
     else:
-        min_eval = math.inf
-        for child in pos:
+        min_eval = float("inf")
+        for child in board:
             evaluation = minimax(child, depth - 1, alpha, beta, True)
             min_eval = min(min_eval, evaluation)
             beta = min(beta, evaluation)
